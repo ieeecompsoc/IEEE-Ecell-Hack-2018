b8c5cac2268bc21b3bd70c465b27a91df9444f32
sahilkhurana19 <sahil_khurana@outlook.com>
2018-01-22 00:18:11 +0530

    Added logos and parallax for about us

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@
 particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+$('.parallax-window').parallax({
+	imageSrc: 'images/AboutUS.jpg',
+	speed: 0.2,
+});
+
