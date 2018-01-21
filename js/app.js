9a4c2ef0090d1399a5933135cae6ec35adf367d3
sahilkhurana19 <sahil_khurana@outlook.com>
2018-01-22 00:41:11 +0530

    Added parallax to faqs

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@
 particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
-$('.parallax-window').parallax({
+$('.about-parallax-window').parallax({
 	imageSrc: 'images/AboutUS.jpg',
+	speed: 0.2,
+});
+$('.faq-parallax-window').parallax({
+	imageSrc: 'images/FAQs.jpg',
 	speed: 0.2,
 });
 
