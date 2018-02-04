54355558907979f04553ca201e469c917bf946d1
ankushgarg1998 <1998anky@gmail.com>
2018-02-05 02:03:18 +0530

    Timer Added

--- js/app.js
+++ js/app.js
@@ -11,3 +11,35 @@ $('.about-parallax-window').parallax({
 // 	speed: 0.2,
 // });
 
+
+activateTimer = function(date) {
+	const endDate = new Date("Feb 17, 2018 09:00:00").getTime(),
+	$timer = document.getElementById("js-timer");
+
+	let days, hours, minutes;
+
+	const intervalId = setInterval(() => {
+		const currentTime = new Date().getTime(),
+			distance = endDate - currentTime;
+
+		days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+		const timeLeft = `${days} days : ${hours} hours : ${minutes} minutes`;
+
+		if (timeLeft !== $timer.innerHTML) {
+			$timer.innerHTML = timeLeft;
+		}
+
+		if (distance < 0) {
+			clearInterval(intervalId);
+
+			$timer.innerHTML = "0 days : 0 hours : 0 minutes";
+		}
+	}, 1000);
+}
+
+window.onload = function() {
+	activateTimer();
+}
